3bf238b62879a4d0902b2380158456e60d1f1b6b
Robert K. Bell <robert.k.bell@gmail.com>
2026-08-08 12:29:12 +1000

    style: switch from deprecated to modern nixfmt

--- nix/default.nix
+++ nix/default.nix
@@ -4,16 +4,29 @@
 # manually replaced the next two lines:
 # { nixpkgs ? <nixpkgs>, config ? { } }:
 # with (import nixpkgs config);
-{ elmPackages, lib, pkgs, minimalElmSrc, stdenv, uglify-js }:
+{
+  elmPackages,
+  lib,
+  pkgs,
+  minimalElmSrc,
+  stdenv,
+  uglify-js,
+}:
 let
-  mkDerivation = { srcs ? ./elm/elm-srcs.nix, src, name, srcdir ? "../src"
-    , targets ? [ ], registryDat ? ./elm/registry.dat, outputJavaScript ? false
+  mkDerivation =
+    {
+      srcs ? ./elm/elm-srcs.nix,
+      src,
+      name,
+      srcdir ? "../src",
+      targets ? [ ],
+      registryDat ? ./elm/registry.dat,
+      outputJavaScript ? false,
     }:
     stdenv.mkDerivation {
       inherit name src;
 
-      buildInputs = [ elmPackages.elm ]
-        ++ lib.optional outputJavaScript uglify-js;
+      buildInputs = [ elmPackages.elm ] ++ lib.optional outputJavaScript uglify-js;
 
       buildPhase = pkgs.elmPackages.fetchElmDeps {
         elmPackages = import srcs;
@@ -21,26 +34,28 @@ let
         inherit registryDat;
       };
 
-      installPhase = let
-        elmfile = module:
-          "${srcdir}/${builtins.replaceStrings [ "." ] [ "/" ] module}.elm";
-        extension = if outputJavaScript then "js" else "html";
-      in ''
-        mkdir -p $out/share/doc
-        ${lib.concatStrings (map (module: ''
-          echo "compiling ${elmfile module}"
-          elm make ${
-            elmfile module
-          } --output $out/${module}.${extension} --docs $out/share/doc/${module}.json
-          ${lib.optionalString outputJavaScript ''
-            echo "minifying ${elmfile module}"
-            uglifyjs $out/${module}.${extension} --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' \
-                | uglifyjs --mangle --output $out/${module}.min.${extension}
-          ''}
-        '') targets)}
-      '';
+      installPhase =
+        let
+          elmfile = module: "${srcdir}/${builtins.replaceStrings [ "." ] [ "/" ] module}.elm";
+          extension = if outputJavaScript then "js" else "html";
+        in
+        ''
+          mkdir -p $out/share/doc
+          ${lib.concatStrings (
+            map (module: ''
+              echo "compiling ${elmfile module}"
+              elm make ${elmfile module} --output $out/${module}.${extension} --docs $out/share/doc/${module}.json
+              ${lib.optionalString outputJavaScript ''
+                echo "minifying ${elmfile module}"
+                uglifyjs $out/${module}.${extension} --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' \
+                    | uglifyjs --mangle --output $out/${module}.min.${extension}
+              ''}
+            '') targets
+          )}
+        '';
     };
-in mkDerivation {
+in
+mkDerivation {
   name = "j2etv-elm2nix-0.1.0";
   srcs = ./elm/elm-srcs.nix;
   src = minimalElmSrc;
